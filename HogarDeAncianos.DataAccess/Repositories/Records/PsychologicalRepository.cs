12182101751d461ac117a906982c7c24c957deab
Tatiana Fernández <eytati050@hotmail.com>
2017-09-06 22:45:05 -0600

    Se creo toda la funcionalidad de expediente psicologico

---
+++ HogarDeAncianos.DataAccess/Repositories/Records/PsychologicalRepository.cs
@@ -0,0 +1,159 @@
+﻿using HogarDeAncianos.Bussiness.Entities.Records;
+using HogarDeAncianos.Bussiness.IRepositories;
+using HogarDeAncianos.DataAccess.Connection;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogarDeAncianos.DataAccess.Repositories.Records
+{
+    public class PsychologicalRepository : IRepository<Psychological>
+    {
+        protected static IMongoDatabase _dataBase;
+        public PsychologicalRepository()
+        {
+            ConnectionDB classConnection = new ConnectionDB();
+            _dataBase = classConnection.Get_dataBase();
+
+        }
+
+        public IMongoCollection<BsonDocument> ObtenerColeccion()
+        {
+            return _dataBase.GetCollection<BsonDocument>("PsychologicalRecordDB");
+        }
+
+        public Psychological CreateOneDocument(Psychological item)
+        {
+            BsonDocument empleado = new BsonDocument
+            {
+                { "Id", item.Id },
+                { "Cedula" , item.Identification },
+                { "Mental test" , item.MentalTest},
+                { "Monitoreo" , item.Monitoring},
+                { "Historia Personal" , item.PersonalHistory},
+                { "Examen Psicologico" , item.PsychologicalTest},
+                { "Observaciones" , item.Observations},
+                { "Creado por" , item.CreatedByUser},
+                { "Editado" , item.EditedByUser},
+                { "Creado en" , item.CreationTime},
+                { "Editado en" , item.EditionTime}
+
+            };
+
+            IMongoCollection<BsonDocument> collection = ObtenerColeccion();
+            collection.InsertOneAsync(empleado);
+            return item;
+        }
+
+        public bool DeleteOneDocument(string id)
+        {
+            try
+            {
+                IMongoCollection<BsonDocument> collection = ObtenerColeccion();
+                collection.DeleteManyAsync(Builders<BsonDocument>.Filter.Eq("Cedula", id));
+                return true;
+            }
+            catch {
+                return false; 
+            }
+        }
+
+        public async Task<IEnumerable<Psychological>> GetManyDocumentsAsync()
+        {
+            IMongoCollection<BsonDocument> collection = ObtenerColeccion();
+            List<BsonDocument> EmployeeListBson = new List<BsonDocument>();
+            List<Psychological> RecordList = new List<Psychological>();
+            try
+            {
+                await collection.Find(new BsonDocument()).ForEachAsync(X => EmployeeListBson.Add(X));
+
+                foreach (BsonDocument documento in EmployeeListBson)
+                {
+                    var data = documento.ToList();
+                    Psychological record = new Psychological
+                    {
+                        Id = data[1].Value.AsGuid,
+                        Identification = data[2].Value.ToString(),
+                        MentalTest = data[3].Value.ToString(),
+                        Monitoring = data[4].Value.ToString(),
+                        PersonalHistory = data[5].Value.ToString(),
+                        PsychologicalTest = data[6].Value.ToString(),
+                        Observations = data[7].Value.ToString(),
+                        CreatedByUser = data[8].Value.ToString(),
+                        EditedByUser = data[9].Value.ToString(),
+                        CreationTime = data[10].Value.ToUniversalTime(),
+                        EditionTime = data[11].Value.ToUniversalTime()
+
+                    };
+
+                    RecordList.Add(record);
+                }
+            }
+            catch (Exception)
+            { }
+            return RecordList;
+        }
+
+        public async Task<Psychological> GetOneDocument(string id)
+        {
+            Psychological record = null;
+            try
+            {
+                IMongoCollection<BsonDocument> collection = ObtenerColeccion();
+                var filter = Builders<BsonDocument>.Filter.Eq("Cedula", id);
+                var result = await collection.Find(filter).ToListAsync();
+
+                var data = result[0].ToList();
+                record = new Psychological
+                {
+                    Id = data[1].Value.AsGuid,
+                    Identification = data[2].Value.ToString(),
+                    MentalTest = data[3].Value.ToString(),
+                    Monitoring = data[4].Value.ToString(),
+                    PersonalHistory = data[5].Value.ToString(),
+                    PsychologicalTest = data[6].Value.ToString(),
+                    Observations = data[7].Value.ToString(),
+                    CreatedByUser = data[8].Value.ToString(),
+                    EditedByUser = data[9].Value.ToString(),
+                    CreationTime = data[10].Value.ToUniversalTime(),
+                    EditionTime = data[11].Value.ToUniversalTime()
+                };
+
+
+            }
+            catch (Exception)
+            {
+                record = null; 
+            }
+            return record;
+        }
+
+        public bool UpdateOneDument(string id, Psychological item)
+        {
+            IMongoCollection<BsonDocument> collection = ObtenerColeccion();
+
+            BsonDocument record = new BsonDocument
+            {
+                { "Id", item.Id },
+                { "Cedula" , item.Identification },
+                { "Mental test" , item.MentalTest},
+                { "Monitoreo" , item.Monitoring},
+                { "Historia Personal" , item.PersonalHistory},
+                { "Examen Psicologico" , item.PsychologicalTest},
+                { "Observaciones" , item.Observations},
+                { "Creado por" , item.CreatedByUser},
+                { "Editado" , item.EditedByUser},
+                { "Creado en" , item.CreationTime},
+                { "Editado en" , item.EditionTime}
+            };
+
+            collection.ReplaceOneAsync(new BsonDocument("Cedula", item.Identification), new BsonDocument(record));
+
+            return true;
+        }
+    }
+}
---
+++ HogarDeAncianos/Controllers/PsychologicalController.cs
@@ -0,0 +1,157 @@
+﻿using HogarDeAncianos.Bussiness.Entities.Records;
+using HogarDeAncianos.DataAccess.Repositories.Records;
+using HogarDeAncianos.Models;
+using HogarDeAncianos.ParametersObjects.Records;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HogarDeAncianos.Controllers
+{
+    public class PsychologicalController : Controller
+    {
+
+        private PsychologicalRepository records = new PsychologicalRepository();
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            IEnumerable<Psychological> recordsIEnumerable = await records.GetManyDocumentsAsync();
+            List<PsychologicalViewModel> vista = new List<PsychologicalViewModel>();
+
+
+            foreach (var record in recordsIEnumerable)
+            {
+                PsychologicalViewModel model = new PsychologicalViewModel
+                {
+                    Identification = record.Identification,
+                    MentalTest = record.MentalTest,
+                    PersonalHistory = record.PersonalHistory,
+                    Monitoring = record.Monitoring,
+                    PsychologicalTest = record.PsychologicalTest,
+                    Observations = record.Observations,
+                    CreatedByUser = record.CreatedByUser,
+                    EditedByUser = record.EditedByUser,
+                    CreationTime = record.CreationTime,
+                    EditionTime = record.EditionTime
+                };
+
+                vista.Add(model);
+            }
+            return View(vista);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(PsychologicalViewModel modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                PsychologicalParameter record = modelo;
+                Psychological newRecord = new Psychological(record);
+                records.CreateOneDocument(newRecord);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Algunos de los campos requeridos no esta presente");
+                return View(modelo);
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Details(string id)
+        {
+            Psychological record = await records.GetOneDocument(id);
+            if (record != null)
+            {
+                PsychologicalViewModel model = new PsychologicalViewModel
+                {
+                    Identification = record.Identification,
+                    MentalTest = record.MentalTest,
+                    PersonalHistory = record.PersonalHistory,
+                    Monitoring = record.Monitoring,
+                    PsychologicalTest = record.PsychologicalTest,
+                    Observations = record.Observations,
+                    CreatedByUser = record.CreatedByUser,
+                    EditedByUser = record.EditedByUser,
+                    CreationTime = record.CreationTime,
+                    EditionTime = record.EditionTime
+                };
+
+                return View(model);
+            }
+            else
+            {
+                ViewBag.ErrirMessage = "El id de ensayo ingresado no fue encontrado";
+                return View("Error");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Delete(string id)
+        {
+            if (records.DeleteOneDocument(id))
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.ErrirMessage = "No se puede eliminar el expediente";
+                return View("Error");
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(string id)
+        {
+            Psychological record = await records.GetOneDocument(id);
+            PsychologicalViewModel model = new PsychologicalViewModel
+            {
+                Identification = record.Identification,
+                MentalTest = record.MentalTest,
+                PersonalHistory = record.PersonalHistory,
+                Monitoring = record.Monitoring,
+                PsychologicalTest = record.PsychologicalTest,
+                Observations = record.Observations,
+                CreatedByUser = record.CreatedByUser,
+                EditedByUser = record.EditedByUser,
+                CreationTime = record.CreationTime,
+                EditionTime = record.EditionTime
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(string id, PsychologicalViewModel modelo)
+        {
+            {
+                if (ModelState.IsValid)
+                {
+
+                    PsychologicalParameter record = modelo;
+                    Psychological employee = new Psychological(record);
+                    records.UpdateOneDument(id, employee);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Algunos de los campos requeridos no esta presente");
+                    return View(modelo);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
