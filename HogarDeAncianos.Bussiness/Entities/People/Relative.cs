dc2c5495e31c228072eb72fbdc0e5feb998c0b30
Tatiana Fernández <eytati050@hotmail.com>
2017-09-10 23:29:26 -0600

    Create About Relative
    Add Views, model, entity, parameter, repository and controller

---
+++ HogarDeAncianos.Bussiness/Entities/People/Relative.cs
@@ -0,0 +1,36 @@
+﻿using HogarDeAncianos.Bussiness.Entities.People;
+using HogarDeAncianos.ParametersObjects.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogarDeAncianos.Bussiness.Entities.Records
+{
+    public class Relative: Person 
+    {
+        public Relative(RelativeParameter parameter)
+        {
+            this.Address = parameter.Address;
+            this.Email = parameter.Email;
+            this.Name = parameter.Name;
+            this.FirstSurname = parameter.FirstSurname;
+            this.SecondSurname = parameter.SecondSurname;
+            this.Identification = parameter.Identification;
+            this.Phone = parameter.Phone;
+            this.Related = parameter.Related;
+            this.OlderAdultId = parameter.OlderAdultId;
+        }
+
+        public Relative() { }
+
+        public string Related { set; get; }
+
+        public string Phone { set; get; }
+
+        public string OlderAdultId { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ HogarDeAncianos.ParametersObjects/People/RelativeParameter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogarDeAncianos.ParametersObjects.People
+{
+    public abstract class RelativeParameter
+    {
+        public abstract string Identification { set; get; }
+
+        public abstract string Name { set; get; }
+
+        public abstract string FirstSurname { set; get; }
+
+        public abstract string SecondSurname { set; get; }
+
+        public abstract string Address { set; get; }
+
+        public abstract string Related { set; get; }
+
+        public abstract string Phone { set; get; }
+
+        public abstract string OlderAdultId { get; set; }
+
+        public abstract string Email { get; set; }
+    }
+}
